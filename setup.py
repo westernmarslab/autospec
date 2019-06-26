d424dea12f4af3e1d227b2c449c5b7d5819a873f
westernmarslab <hozak@wwu.edu>
2019-06-26 16:26:51 -0700

    no changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='AutoSpec',
-    version='0.0.45',
+    version='0.0.46',
     packages=['autospec'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='Control software for spectroscopy using ASD RS3 and ViewSpec Pro',
